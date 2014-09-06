0b28d4749ed47e411c3df1d61934936d1209cb88
rvilar <ramon@ymbra.com>
2014-09-06 13:26:24 +0200

    Fixed directory location error on installation script file

--- drupalcatinstall.sh
+++ drupalcatinstall.sh
@@ -5,5 +5,6 @@ rm -rf docroot
 drush -y make drupalcat.make docroot
 # Copy install profile to the docroot
 cp -a drupalcat docroot/profiles
-drush -y si --db-url=mysql://user:pass@localhost/test --account-pass=admin drupalcat
+cd docroot
+drush -y si --db-url=mysql://user:pass@localhost/drupalcat --account-pass=admin drupalcat
 drush uli
