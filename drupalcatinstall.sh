098c8a67e830ea7e838b62bb46cb91188791e9b3
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2014-09-06 13:11:19 +0200

    Issue #10 - Create drupacat minimal install profile

--- drupalcatinstall.sh
+++ drupalcatinstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-drush -y make drupalcat.make
-drush -y si --db-url=mysql://user:pass@localhost/test --account-pass=admin standard
+# Make sure there is no docroot.
+chmod -R 777 docroot
+rm -rf docroot
+drush -y make drupalcat.make docroot
+# Copy install profile to the docroot
+cp -a drupalcat docroot/profiles
+drush -y si --db-url=mysql://user:pass@localhost/test --account-pass=admin drupalcat
 drush uli
