6d5190b11314af9f0b096f5e6bfd6084ba66432e
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2014-10-18 14:04:38 +0200

    Use soflink instead of complete copy for the install profile

--- drupalcatinstall.sh
+++ drupalcatinstall.sh
@@ -4,10 +4,11 @@ chmod -R 777 docroot
 rm -rf docroot
 # Download drupal core and contrib modules.
 drush -y make drupalcat.make docroot
-# Copy install profile to the docroot.
-cp -a drupalcat docroot/profiles
-# Change dir before doing install.
-cd docroot
+# Copy install profile to the docroot. Is preferable to use soft links.
+#cp -a drupalcat docroot/profiles
+# Soft link install profile to the docroot.
+cd docroot/profiles
+ln -s ../../drupalcat drupalcat
 # Here you have to change user/pass to match your mysql user
 drush -y si --db-url=mysql://user:pass@localhost/drupalcat  --locale=ca --account-pass=admin drupalcat
 # login as admin after the install.
