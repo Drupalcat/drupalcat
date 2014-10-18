b4310a32c3227a84fe19b08b4c3afbc592b1b4ea
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2014-10-18 11:35:01 +0200

    Task #8 - update helper sh script to do the install in catalonian and more comments for each step

--- drupalcatinstall.sh
+++ drupalcatinstall.sh
@@ -2,9 +2,13 @@
 # Make sure there is no docroot.
 chmod -R 777 docroot
 rm -rf docroot
+# Download drupal core and contrib modules.
 drush -y make drupalcat.make docroot
-# Copy install profile to the docroot
+# Copy install profile to the docroot.
 cp -a drupalcat docroot/profiles
+# Change dir before doing install.
 cd docroot
-drush -y si --db-url=mysql://user:pass@localhost/drupalcat --account-pass=admin drupalcat
+# Here you have to change user/pass to match your mysql user
+drush -y si --db-url=mysql://user:pass@localhost/drupalcat  --locale=ca --account-pass=admin drupalcat
+# login as admin after the install.
 drush uli
